9180bcc72d444b7fd34cda0e052a5ae5b93e949d
darkmatter11235 <darkmatter11235@github.com>
2018-11-19 21:50:47 -0800

    Training with 10000 epochs

--- ConvAE.py
+++ ConvAE.py
@@ -2,7 +2,7 @@ from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
 from keras.datasets import mnist
 import numpy as np
 import matplotlib.pyplot as plt
-from keras.models import Model
+from keras.models import Model, load_model
 from utils import *
 
 #image_width = 28
@@ -14,7 +14,7 @@ image_height = 120
 num_channels = 1
 #num_channels = 3
 
-num_epochs = 1000
+num_epochs = 10000
 
 # input layer will be image of shape (28,28,1)
 
@@ -88,7 +88,6 @@ x_test = x_test.reshape(len(x_test), image_width, image_height, num_channels)
 #x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))  # adapt this if using `channels_first` image data format
 
 # from keras.callbacks import TensorBoard
-
 autoencoder.fit(x_train, x_train,
                 epochs=num_epochs,
                 batch_size=256,
@@ -101,9 +100,18 @@ autoencoder.fit(x_train, x_train,
 #encoded_images = encoder.predict(x_test)
 #print(encoded_images.shape)
 #decoded_images = decoder.predict(encoded_images)
+"""
+
+del autoencoder
+import h5py
+
+f = h5py.File('convAE_1000.h5', 'r')
+print(f.attrs.get('keras_version'))
+autoencoder = load_model("convAE_1000.h5")
+"""
 decoded_images = autoencoder.predict(x_test)
 
-autoencoder.save("./convAE_"+str(num_epochs)+".h5")
+# autoencoder.save("./convAE_"+str(num_epochs)+".h5")
 
 n = 10
 
@@ -114,6 +122,7 @@ for i in range(n):
     if num_channels > 1 :
         plt.imshow(x_test[i].reshape(image_height, image_width, num_channels))
     else:
+        plt.gray()
         plt.imshow(x_test[i].reshape(image_height, image_width))
     # plt.gray()
     ax.get_xaxis().set_visible(False)
@@ -124,6 +133,7 @@ for i in range(n):
     if num_channels > 1 :
         plt.imshow(decoded_images[i].reshape(image_height, image_width, num_channels))
     else:
+        plt.gray()
         plt.imshow(decoded_images[i].reshape(image_height, image_width))
     # plt.gray()
     ax.get_xaxis().set_visible(False)
